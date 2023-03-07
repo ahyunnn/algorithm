1c809aa0f8f8e4547c22e3fcc9e70750bc0507e1
ahyunnn <ahyun3089@gmail.com>
2023-03-07 23:06:42 +0900

    230307

---
+++ 백준/Silver/쇠막대기.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open("input.txt")
+
+lst = list(input())
+stack = []
+ans = 0
+
+for i in range(len(lst)):
+    if lst[i] == "(":
+        stack.append("(")
+
+    else:
+        if lst[i-1] == "(":
+            stack.pop()
+            ans += len(stack)
+
+        else:
+            stack.pop()
+            ans += 1
+
+print(ans)
