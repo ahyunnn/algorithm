d46e4499e0569a3eae42a0ddf81d477b866f87b2
ahyunnn <ahyun3089@gmail.com>
2023-04-15 23:50:21 +0900

    230415

---
+++ 백준/Bronze/bj_10808(알파벳개수).py
@@ -0,0 +1,13 @@
+import sys
+
+word = list(input())
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+answer = [0] * 26
+
+for a in range(len(alpha)):
+    for i in word:
+        if alpha[a] == i:
+            answer[a] += 1
+
+print(*answer)
