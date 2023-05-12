2ad589f1998b487466acd35aea90a87f3a9ebd1d
ahyunnn <ahyun3089@gmail.com>
2023-05-12 23:35:58 +0900

    230512

---
+++ 백준/Bronze/bj_1978(소수찾기).py
@@ -0,0 +1,35 @@
+# import sys
+# sys.stdin = open('input.txt')
+#
+# def isPrime(n):
+#     for j in range(2, n):
+#         if n % j == 0:
+#             return False
+#         else:
+#             return True
+#
+# n = int(input())
+# lst = list(map(int, input().split()))
+# answer = 0
+# 
+# for i in range(n):
+#     if isPrime(lst[i]):
+#         answer += 1
+#
+# print(answer)
+
+import sys
+sys.stdin = open('input.txt')
+
+n = int(input())
+lst = list(map(int, input().split()))
+answer = 0
+
+for i in lst:
+    for j in range(2, i+1):
+        if i % j == 0:
+            if i == j:
+                answer += 1
+            break
+
+print(answer)
