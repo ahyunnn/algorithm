52798b46d5eaf28f95eff9864288414ac3aa5e02
ahyunnn <ahyun3089@gmail.com>
2023-06-15 21:52:45 +0900

    230615 - programmers - 로그인 성공? 수정

--- 프로그래머스/코딩테스트입문/로그인성공.js
+++ 프로그래머스/코딩테스트입문/로그인성공.js
@@ -1,24 +1,11 @@
 function solution(id_pw, db) {
   let answer = "";
 
-  // for문을 사용하면 마지막 값을 기준으로 result가 도출된다.
-  // 하지만 map을 사용하면 값이 나왔을 때 break가 되는건가?
-  db.map((item) => {
-    if (id_pw[0] === item[0] && id_pw[1] === item[1]) answer = "login";
-    else if (id_pw[0] === item[0] && id_pw[1] !== item[1]) answer = "wrong pw";
-    else if (id_pw[0] !== item[0] && id_pw[1] !== item[1]) answer = "fail";
-  });
+  for (let [id, pw] of db) {
+    if (id_pw[0] === id && id_pw[1] === pw) answer = "login";
+    else if (id_pw[0] === id && id_pw[1] !== pw) answer = "wrong pw";
+    else if (id_pw[0] !== id && id_pw[1] !== pw) answer = "fail";
+  }
 
   return answer;
 }
-
-console.log(
-  solution(
-    ["rabbit04", "98761"],
-    [
-      ["jaja11", "98761"],
-      ["krong0313", "29440"],
-      ["rabbit00", "111333"],
-    ]
-  )
-);
