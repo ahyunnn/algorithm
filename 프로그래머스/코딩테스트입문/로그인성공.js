8d5398bca580bb889452470e6fefc8a83c7f50ab
ahyunnn <ahyun3089@gmail.com>
2023-06-15 21:52:24 +0900

    230615 - programmers - 로그인 성공? 수정

--- 프로그래머스/코딩테스트입문/로그인성공.js
+++ 프로그래머스/코딩테스트입문/로그인성공.js
@@ -1,15 +1,15 @@
 function solution(id_pw, db) {
-  let result = "";
+  let answer = "";
 
-  //2중 for문을 사용하지 않으려고 map으로 했다 !
-  //뭔가 조건문을 더 줄일수 있을 것 같은데,, 어떻게 줄일 수 있을지 싱크빅이 안떠오른다.. 힇희...
-  db.map((item, i) => {
-    if (id_pw[0] === item[0] && id_pw[1] === item[1]) result = "login";
-    else if (id_pw[0] === item[0] && id_pw[1] !== item[1]) result = "wrong pw";
-    else if (id_pw[0] !== item[0] && id_pw[1] !== item[1]) result = "fail";
+  // for문을 사용하면 마지막 값을 기준으로 result가 도출된다.
+  // 하지만 map을 사용하면 값이 나왔을 때 break가 되는건가?
+  db.map((item) => {
+    if (id_pw[0] === item[0] && id_pw[1] === item[1]) answer = "login";
+    else if (id_pw[0] === item[0] && id_pw[1] !== item[1]) answer = "wrong pw";
+    else if (id_pw[0] !== item[0] && id_pw[1] !== item[1]) answer = "fail";
   });
 
-  return result;
+  return answer;
 }
 
 console.log(
