ea3c291ed402a9d85a11ba87c93b2912492a3974
ahyunnn <ahyun3089@gmail.com>
2023-06-18 18:21:46 +0900

    230618 - javascript - 올바른 괄호

---
+++ javascript/섹션6/1.올바른괄호.js
@@ -0,0 +1,18 @@
+function solution(str) {
+  let answer = "YES";
+  let stack = [];
+
+  for (let x of str) {
+    if (x === "(") stack.push(x);
+    else {
+      // stack에 값이 없으면 -> false
+      if (stack.length === 0) return "NO";
+      // stack에 값이 있으면
+      stack.pop();
+    }
+  }
+
+  if (stack.length > 0) return "NO";
+
+  return answer;
+}
