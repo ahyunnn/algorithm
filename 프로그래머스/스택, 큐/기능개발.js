48a9614683087b9e3a180843c5ee52b359123e0c
ahyunnn <ahyun3089@gmail.com>
2023-08-16 23:51:52 +0900

    230816 - programmers - 기능개발 정답 코드

--- 프로그래머스/스택, 큐/기능개발.js
+++ 프로그래머스/스택, 큐/기능개발.js
@@ -1,27 +1,25 @@
 function solution(progresses, speeds) {
-  var answer = [];
+  var answer = [0];
 
-  let newProgresses = progresses.map((x) => 100 - x);
+  // newProgresses : 남은 일수를 계산
+  let newProgresses = progresses.map((progress, index) =>
+    Math.ceil((100 - progress) / speeds[index])
+  );
 
-  let num = 0;
-  let days = [];
+  // max : 기능개발 시간이 더 걸리는 날
+  let max = newProgresses[0];
 
-  for (let i = 0; i < newProgresses.length; i++) {
-    let day = Math.round(newProgresses[i] / speeds[num]);
-    days.push(day);
-    num++;
-  }
-
-  for (let j = 0; j < days.length; j++) {
-    let count = 1;
-    for (let l = j + 1; l < days.length; l++) {
-      if (days[j] >= days[l]) {
-        count++;
-      } else if (days[j] < days[l]) {
-        answer.push(count);
-      } else if (days.length === 1) {
-        answer.push(count);
-      }
+  // newProgresses의 요소들을 순회하면서 max값 기준으로 answer에 값을 차곡차곡 쌓아나가기
+  // i : 순회를 위한 횟수
+  // j : answer의 인덱스 번호
+  for (let i = 0, j = 0; i < newProgresses.length; i++) {
+    // max값보다 작거나 같은 경우에는 누적
+    if (newProgresses[i] <= max) {
+      answer[j] += 1;
+    } else {
+      // max값보다 큰 경우에는 다음날로 넘어가기
+      max = newProgresses[i];
+      answer[++j] = 1;
     }
   }
   return answer;
